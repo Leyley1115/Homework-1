a618d072274ff449ec58281a16885ac7f41a92fd
Leyley1115 <sophia.pris22@gmail.com>
2025-03-24 23:05:53 +0300

    js hw

---
+++ hw-2/kod.js
@@ -0,0 +1,50 @@
+// 1
+let a=10;
+alert(`1.1) ${a}`);
+
+a=20;
+alert(`1.2) ${a}`);
+
+// 2
+let b=2007;
+alert(`2) Год выпуска первого iPhone: ${b}`);
+
+// 3
+let c='Брендан Эйх или Айк';
+alert(`3) ${c} --  создатель языка JavaScript.`);
+
+// 4
+let a1=10, a2=2;
+alert(`4) сумма:${a1+a2}, разность:${a1-a2}, произведение:${a1*a2}, частное:${a1/a2}`)
+
+// 5
+alert(`5) ${result=2**5}`);
+
+// 6
+a=9, b=2;
+alert(`6) ${a%b}`);
+
+// 7
+let num=1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+++num;
+--num;
+alert(`7) ${num}`);
+
+// 8
+let age = prompt('8) Сколько вам лет?');
+alert(age)
+
+// 9
+const user={
+name:'Аля',
+age: 28,
+isAdmin: true
+}
+
+// 10
+let name1=prompt('Как Вас зовут?')
+alert(`Привет, ${name1}!`)
