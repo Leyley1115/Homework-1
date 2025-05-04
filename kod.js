329f4ff15b90a3e984d270ddf97d6b47787fa16e
Leyley1115 <sophia.pris22@gmail.com>
2025-05-04 14:22:27 +0300

    mini-game6

--- kod.js
+++ kod.js
@@ -212,3 +212,16 @@ function one_third(){
         alert(`Компьютер выбрал: ${compVar},\nВы выбрали: ${vars[userVar-1]}.\nРезультат: Вы проиграли`);
     }
 }
+
+// #6
+function change_color(){
+    let colorCode=[];
+
+    for(let i=0; i<6; i++){
+        let colorNumb=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F',];
+        colorCode.push(colorNumb[Math.floor(Math.random()*16)]);
+        
+    }
+
+    document.querySelector('.main_color').style.backgroundColor=`#${colorCode.join('')}`;
+}
